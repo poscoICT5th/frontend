f62a948077b0662e2c0beb564ebe3598d858bf8d
choyeongwoo <choyeongwoo.s@gmail.com>
2022-05-17 20:33:43 +0900

    maindashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
-import React from "react";
-import List1 from "./components/List/List1";
-import Menu from "./components/Basic/Menu";
-=======
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Account/Login'
 import MainDashboard from './components/Dashboard/MainDashboard'
->>>>>>> cho
-
+import Navbar from './components/Basic/Navbar'
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-
-     <Menu />
-      
-    
-=======
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Main" element={<MainDashboard />} />
       </Routes>
->>>>>>> cho
     </div>
   )
 }
