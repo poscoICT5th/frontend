32dcfb5b7ccde94cc7fdc5f380e246d39746ba70
wjddkswl97 <wjddkswl97@gmail.com>
2022-05-18 09:06:52 +0900

    App.js 안지꺼 없애기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import List1 from "./components/List/List1";
-import Menu from "./components/Basic/Menu";
-import Forms from "./components/List/Forms";
+
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Account/Login'
 import MainDashboard from './components/Dashboard/MainDashboard'
