b41b7ae02155e6dc5703be1a9b9a5ec3ba39b839
choyeongwoo <choyeongwoo.s@gmail.com>
2022-05-18 09:04:19 +0900

    안지씨 js들 App.js에서는 없애버림

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import List1 from "./components/List/List1";
-import Forms from "./components/List/Forms";
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Account/Login'
 import MainDashboard from './components/Dashboard/MainDashboard'
