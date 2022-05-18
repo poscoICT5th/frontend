868c88015737fd0f0b453427e610062b94b9f50d
choyeongwoo <choyeongwoo.s@gmail.com>
2022-05-18 20:42:17 +0900

    Search부분에서 물품관리 나누기

--- frontend/src/components/List/Junior/RequestProductListJunior.js
+++ frontend/src/components/List/Junior/RequestProductListJunior.js
@@ -89,21 +89,21 @@ function RequestProductListJunior() {
                             By Email
                         </div>
                         <div className="mt-4 space-y-4">
-                            <div className="flex items-start">
-                                <div className="flex items-center h-5">
-                                    <input
-                                        id="comments"
-                                        name="comments"
-                                        type="checkbox"
-                                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                    />
-                                </div>
-                                <div className="ml-3 text-sm">
-                                    <label htmlFor="comments" className="font-medium text-gray-700">
-                                        Comments
-                                    </label>
-                                    <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                                </div>
+                            <div className="">
+                                <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                                    Product
+                                </label>
+                                <select
+                                    id="country"
+                                    name="country"
+                                    autoComplete="country-name"
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                >
+                                    <option>전체보기</option>
+                                    <option>입고</option>
+                                    <option>출고</option>
+                                    <option>창고이동</option>
+                                </select>
                             </div>
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
