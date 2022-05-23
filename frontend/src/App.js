3a3056cc2a26737b613a362bba14a5b7e0b55cfc
wjddkswl97 <wjddkswl97@gmail.com>
2022-05-23 14:27:55 +0900

    sidebar1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,58 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom'
-import Login from './components/Account/Login'
-import Navbar from './components/Basic/Navbar'
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Account/Login";
+import Navbar from "./components/Basic/Navbar";
 import MainDashboardJunior from "./components/Dashboard/Junior(X)/MainDashboardJunior";
 import MainDashboardOffice from "./components/Dashboard/Office/MainDashboardOffice";
 import WarehouseList from "./components/List/Common/WarehouseList";
 import StockList from "./components/List/Common/StockList";
 import ProductList from "./components/List/Common/ProductList";
 import ProductDetail from "./components/Detail/ProductDetail";
-import Map1 from './components/Map/Map1'
+import Map1 from "./components/Map/Map1";
 import PreparingPage from "./components/Basic/PreparingPage";
-
+import Sidebar1 from "./components/Basic/Sidebar1";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Login />} />
-        <Route path="/WarehouseList" exact element={<WarehouseList />} />
-        <Route path="/StockList" exact element={<StockList />} />
-        <Route path="/productdetail/:lot" exact element={<ProductDetail />} />
-        <Route path="/ProductList" element={<ProductList />} />
-        <Route path="/PreparingPage" exact element={<PreparingPage />} />
-        {/* Junior */}
-        <Route path="/MainDashboardJunior" element={<MainDashboardJunior />} />
-        {/* Office */}
-        <Route path="/MainDashboardOffice" element={<MainDashboardOffice />} />
-        {/* anzi */}
-        <Route path="/map1" element={<Map1 />} />
-      </Routes>
+      <Layout>
+        <Sider>
+          <Sidebar1 />
+        </Sider>
+        <Layout>
+          <Content>
+            <Routes>
+              <Route path="/" exact element={<Login />} />
+              <Route path="/WarehouseList" exact element={<WarehouseList />} />
+              <Route path="/StockList" exact element={<StockList />} />
+              <Route
+                path="/productdetail/:lot"
+                exact
+                element={<ProductDetail />}
+              />
+              <Route path="/ProductList" element={<ProductList />} />
+              <Route path="/PreparingPage" exact element={<PreparingPage />} />
+              {/* Junior */}
+              <Route
+                path="/MainDashboardJunior"
+                element={<MainDashboardJunior />}
+              />
+              {/* Office */}
+              <Route
+                path="/MainDashboardOffice"
+                element={<MainDashboardOffice />}
+              />
+              {/* anzi */}
+              <Route path="/map1" element={<Map1 />} />
+            </Routes>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
